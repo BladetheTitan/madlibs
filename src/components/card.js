9c02af588100c0bfc708cca1d4d7976f7a3d522c
Evan Norland <bladethetitan@gmail.com>
2018-07-10 17:11:57 +0000

    2 way binding with input on card

--- src/components/card.js
+++ src/components/card.js
@@ -3,9 +3,36 @@ import React, { Component } from 'react';
 import Input from './input';
 
 class Card extends Component {
+    
+    constructor() {
+        super()
+        
+        this.state = {
+            color: 'BLUE',
+            pluralNoun: '',
+        }
+        
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    
+    handleInputChange() {
+        console.log('this is the value');
+        this.setState({ color: 'red' })
+    }
+    
+    
     render() {
+        
         return (
             <div className="card">
+                <h1>{this.state.color}</h1>
+                <input onChange={this.handleInputChange}/>
+            </div>
+        )
+        
+        return (
+            <div className="card">
+                <h1>{this.state.color}</h1>
                 { Input('Color') }
                 { Input('Plural Noun') }
             </div>
